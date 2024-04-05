4b6be4295193b66a344b3d38c4558b617effedb4
Thinz <caodangthinh1@gmail.com>
2024-04-06 02:18:44 +0700

    first commit

---
+++ database/migrations/2024_04_05_191100_create_users_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('phone');
+            $table->text('address');
+            $table->string('answer');
+            $table->integer('role')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
